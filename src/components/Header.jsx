fd820104762a6ca73c2304583000e83f5b82cb71
Sajjad Rahimi <sajjad021.5555@gmail.com>
2024-05-21 19:44:52 +0330

    add header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
+// rfce
 
 const courseStatus = ["All", "Active", "Completed", "Upcoming"];
 
 function Header() {
   return (
     <div>
-      <h1>My Courses (3)</h1>
+      <h1 className="">My Coursec (3)</h1>
       <div className="course-status">
         {courseStatus.map((s) => (
-          <div key={s}>{s}</div>
+          <div key={s} className={`${s === "All" ? "active" : ""}`}>
+            {s}
+          </div>
         ))}
       </div>
     </div>
